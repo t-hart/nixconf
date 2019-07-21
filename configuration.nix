bdd4760c81ee7913bdacbda60274267f4d79fd40
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2019-07-21 15:01:59 +0200

    adds udev rules for ergodox app, disables kb

--- configuration.nix
+++ configuration.nix
@@ -217,6 +217,35 @@ in
       };
     };
 
+    services.udev.extraRules = ''
+        # Teensy rules for the Ergodox EZ Original / Shine / Glow
+        ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+        ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+        KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+        # STM32 rules for the Planck EZ Standard / Glow
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+            MODE:="0666", \
+            SYMLINK+="stm32_dfu"
+    '';
+
+
+    # systemd.user.services.kb = {
+    #   enable = false;
+    #   description = "keyboard: layout tweaks and xcape";
+    #   wantedBy = [ "graphical.target" "default.target" ];
+    #   preStart = ''
+    #     ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY
+    #   '';
+    #   restartIfChanged = true;
+    #   serviceConfig = {
+    #     Type = "forking";
+    #     Restart = "always";
+    #     RestartSec = 2;
+    #     ExecStart = "${pkgs.xcape}/bin/xcape -t 250 -e \'Shift_L=dollar;Shift_R=numbersign;Control_L=Escape;Control_R=Return\'";
+    #   };
+    # };
 
     systemd.user.services.kb = {
       enable = true;
