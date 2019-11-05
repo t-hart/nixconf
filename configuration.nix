d67e4e7b03d12e1bfa357d6570534b301e2ed14a
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2019-11-05 23:23:26 +0100

    formats file, updates kernel, optimus testing

--- configuration.nix
+++ configuration.nix
@@ -2,267 +2,283 @@
 
 let
   # keyboard
-  compiledLayout = pkgs.runCommand "keyboard-layout" {} ''
+  compiledLayout = pkgs.runCommand "keyboard-layout" { } ''
     ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${/etc/nixos/layout.xkb} $out
   '';
 
-in
-{
-    imports =
-      [ # Include the results of the hardware scan.
-        ./hardware-configuration.nix
-      ];
+in {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-    # Use the systemd-boot EFI boot loader.
-    boot = {
-      loader.systemd-boot.enable = true;
-      loader.efi.canTouchEfiVariables = true;
-      kernelParams = ["acpi_rev_override" "mem_sleep_default=deep" "intel_iommu=igfx_off" ];
-      # kernelPackages = pkgs.linuxPackages_latest;
-      extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    kernelParams =
+      [ "acpi_rev_override" "mem_sleep_default=deep" "intel_iommu=igfx_off" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 
-      # As recommended to avoid kernel panics on Dell XPS 9570 by the arch wiki: https://wiki.archlinux.org/index.php/Dell_XPS_15_9570#Graphics
-      blacklistedKernelModules = [
-        "nouveau"
-        "rivafb"
-        "nvidiafb"
-        "rivatv"
-        "nv"
-        "uvcvideo"
-      ];
+    # As recommended to avoid kernel panics on Dell XPS 9570 by the arch wiki: https://wiki.archlinux.org/index.php/Dell_XPS_15_9570#Graphics
+    blacklistedKernelModules =
+      [ "nouveau" "rivafb" "nvidiafb" "rivatv" "nv" "uvcvideo" ];
 
-      # extraModprobeConfig = ''
-      #   options bbswitch load_state=-1 unload_state=1 nvidia-drm
-      # '';
-
-      tmpOnTmpfs = true;
-    };
-
-    nix.gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
-
-
-    system.autoUpgrade.enable = true;
-
-    networking.networkmanager.enable = true;
-
-    # Select internationalisation properties.
-    i18n = {
-      consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
-      consoleUseXkbConfig = true;
-      defaultLocale = "en_US.UTF-8";
-    };
-
-    fonts = {
-      fonts = with pkgs; [
-              dejavu_fonts
-              fira-code
-              fira-code-symbols
-              ipafont
-              kochi-substitute
-              mplus-outline-fonts
-              powerline-fonts
-      ];
-
-      fontconfig = {
-        ultimate.enable = true;
-        defaultFonts = {
-          monospace = ["DejaVu Sans Mono" "IPAGothic"];
-          sansSerif = ["DejaVu Sans" "IPAPGothic"];
-          serif = ["DejaVu Serif" "IPAPMincho"];
-        };
-      };
-    };
-
-  # Set your time zone.
-    time.timeZone = "Europe/Oslo";
-
-    # List packages installed in system profile. To search, run:
-    # $ nix search wget
-    nixpkgs.config.allowUnfree = true;
-    environment = {
-      systemPackages = with pkgs; [
-        acpi
-        any-nix-shell
-        arandr
-        autorandr
-        bind
-        curl
-        emacs
-        exfat
-        exfat-utils
-        fd
-        fish
-        git
-        ispell
-        killall
-        libinput
-        libinput-gestures
-        networkmanager
-        pciutils
-        powertop
-        tmux
-        unzip
-        vim
-        thunderbolt
-        tree
-        wget
-        xcape
-        xclip
-        xorg.xev
-        xorg.xkbcomp
-        zip
-      ];
-
-      interactiveShellInit = ''
-        alias nixdot='git --git-dir=/etc/nixos/git --work-tree=/etc/nixos/'
-      '';
-    };
-
-    # Enable sound.
-    sound.enable = true;
-
-    hardware.pulseaudio = {
-      enable = true;
-      support32Bit = true;
-      package = pkgs.pulseaudioFull;
-    };
-
-    programs = {
-      fish = {
-        enable = true;
-        promptInit = ''
-          any-nix-shell fish --info-right | source
-        '';
-      };
-      ssh.startAgent = true;
-    };
-
-    # hardware.nvidiaOptimus.disable = true;
-    # hardware.opengl = {
-    #   extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
-    #   extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
-    #   driSupport32Bit = true;
-    # };
-
-    # https://wiki.archlinux.org/index.php/Dell_XPS_15_9570#Letting_bumblebee_automatically_unload_the_kernel_module
-    hardware.bumblebee = {
-     enable = true;
-     driver = "nvidia";
-     pmMethod = "auto";
-     connectDisplay = true;
-    };
-
-    services.xserver = {
-      enable = true;
-      libinput = {
-        enable = true;
-        naturalScrolling = true;
-        disableWhileTyping = true;
-      };
-      exportConfiguration = true;
-      autoRepeatDelay = 250;
-      autoRepeatInterval = 150;
-
-      displayManager.slim = {
-        enable = true;
-        autoLogin = true;
-        defaultUser = "thomas";
-      };
-
-      # desktopManager.xfce.enable = true;
-
-      windowManager.exwm = {
-        enable = true;
-      };
-      windowManager.default = "exwm";
-
-    };
-
-    # This value determines the NixOS release with which your system is to be
-    # compatible, in order to avoid breaking some software such as database
-    # servers. You should change this only after NixOS release notes say you
-    # should.
-    system.stateVersion = "18.09"; # Did you read the comment?
-
-    # users
-    users.extraUsers.thomas = {
-      name = "thomas";
-      group = "users";
-      extraGroups = [
-        "wheel" "disk" "audio" "video" "networkmanager" "systemd-journal" "docker"
-      ];
-      createHome = true;
-      uid = 1000;
-      home = "/home/thomas";
-      shell = pkgs.fish;
-    };
-
-    services.emacs.enable = true;
-    services.emacs.defaultEditor = true;
-
-    services.offlineimap = {
-      enable = true;
-      install = true;
-    };
-
-    services.nixosManual.showManual = true;
-
-    virtualisation.docker = {
-      enable = false;
-      autoPrune = {
-        enable = true;
-        flags = ["--all"];
-      };
-    };
-
-    services.udev.extraRules = ''
-        # Teensy rules for the Ergodox EZ Original / Shine / Glow
-        ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
-        ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
-        KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
-
-        # STM32 rules for the Planck EZ Standard / Glow
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
-            MODE:="0666", \
-            SYMLINK+="stm32_dfu"
-
-        # nvidia CUDA config
-        KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm c $(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 0'"
+    extraModprobeConfig = ''
+      options bbswitch load_state=-1 unload_state=1 nvidia-drm
     '';
 
+    tmpOnTmpfs = true;
+  };
 
-    # systemd.user.services.kb = {
-    #   enable = false;
-    #   description = "keyboard: layout tweaks and xcape";
-    #   wantedBy = [ "graphical.target" "default.target" ];
-    #   preStart = ''
-    #     ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY
-    #   '';
-    #   restartIfChanged = true;
-    #   serviceConfig = {
-    #     Type = "forking";
-    #     Restart = "always";
-    #     RestartSec = 2;
-    #     ExecStart = "${pkgs.xcape}/bin/xcape -t 250 -e \'Shift_L=dollar;Shift_R=numbersign;Control_L=Escape;Control_R=Return\'";
-    #   };
-    # };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 
-    systemd.user.services.autorandrize = {
-      enable = true;
-      description = "Automatically adjust screens when waking up";
-      wantedBy = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
-      after = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        TimeOutSec = "0";
-        ExecStart = "${pkgs.autorandr}/bin/autorandr -c";
+  system.autoUpgrade.enable = true;
+
+  networking.networkmanager.enable = true;
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+    consoleUseXkbConfig = true;
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  fonts = {
+    fonts = with pkgs; [
+      dejavu_fonts
+      fira-code
+      fira-code-symbols
+      ipafont
+      kochi-substitute
+      mplus-outline-fonts
+      powerline-fonts
+    ];
+
+    fontconfig = {
+      ultimate.enable = true;
+      defaultFonts = {
+        monospace = [ "DejaVu Sans Mono" "IPAGothic" ];
+        sansSerif = [ "DejaVu Sans" "IPAPGothic" ];
+        serif = [ "DejaVu Serif" "IPAPMincho" ];
       };
     };
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment = {
+    systemPackages = with pkgs; [
+      acpi
+      any-nix-shell
+      arandr
+      autorandr
+      bind
+      curl
+      emacs
+      exfat
+      exfat-utils
+      fd
+      fish
+      git
+      ispell
+      killall
+      libinput
+      libinput-gestures
+      networkmanager
+      pciutils
+      powertop
+      tmux
+      unzip
+      vim
+      thunderbolt
+      tree
+      wget
+      xcape
+      xclip
+      xorg.xev
+      xorg.xkbcomp
+      zip
+    ];
+
+    interactiveShellInit = ''
+      alias nixdot='git --git-dir=/etc/nixos/git --work-tree=/etc/nixos/'
+    '';
+  };
+
+  # Enable sound.
+  sound.enable = true;
+
+  hardware.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+    package = pkgs.pulseaudioFull;
+  };
+
+  hardware.bluetooth.enable = true;
+
+  programs = {
+    fish = {
+      enable = true;
+      promptInit = ''
+        any-nix-shell fish --info-right | source
+      '';
+    };
+    ssh.startAgent = true;
+    tmux = {
+      enable = true;
+      clock24 = true;
+    };
+  };
+
+  # hardware.nvidiaOptimus.disable = true;
+  # hardware.opengl = {
+  #   extraPackages = [ pkgs.linuxPackages.nvidia_x11.out ];
+  #   extraPackages32 = [ pkgs.linuxPackages.nvidia_x11.lib32 ];
+  #   driSupport32Bit = true;
+  # };
+
+  # https://wiki.archlinux.org/index.php/Dell_XPS_15_9570#Letting_bumblebee_automatically_unload_the_kernel_module
+  hardware.bumblebee = {
+    enable = true;
+    pmMethod = "auto";
+    connectDisplay = true;
+  };
+
+  services.tlp = {
+    enable = true;
+    extraConfig = ''
+      RUNTIME_PM_DRIVER_BLACKLIST="nvidia"
+    '';
+  };
+
+  services.xserver = {
+    enable = true;
+    libinput = {
+      enable = true;
+      naturalScrolling = true;
+      disableWhileTyping = true;
+    };
+    exportConfiguration = true;
+    autoRepeatDelay = 250;
+    autoRepeatInterval = 150;
+
+    displayManager.slim = {
+      enable = true;
+      autoLogin = true;
+      defaultUser = "thomas";
+    };
+
+    # desktopManager.xfce.enable = true;
+
+    windowManager.exwm = { enable = true; };
+    windowManager.default = "exwm";
+
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+  # users
+  users.extraUsers.thomas = {
+    name = "thomas";
+    group = "users";
+    extraGroups = [
+      "audio"
+      "disk"
+      "docker"
+      "networkmanager"
+      "systemd-journal"
+      "video"
+      "wheel"
+    ];
+    createHome = true;
+    uid = 1000;
+    home = "/home/thomas";
+    shell = pkgs.fish;
+  };
+
+  services.emacs.enable = true;
+  services.emacs.defaultEditor = true;
+
+  services.offlineimap = {
+    enable = true;
+    install = true;
+  };
+
+  services.nixosManual.showManual = true;
+
+  virtualisation.docker = {
+    enable = false;
+    autoPrune = {
+      enable = true;
+      flags = [ "--all" ];
+    };
+  };
+
+  services.udev.extraRules = ''
+    # Teensy rules for the Ergodox EZ Original / Shine / Glow
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+    # STM32 rules for the Planck EZ Standard / Glow
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+        MODE:="0666", \
+        SYMLINK+="stm32_dfu"
+
+    # nvidia CUDA config
+    # KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm c $(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 0'"
+
+  '';
+
+  # systemd.user.services.kb = {
+  #   enable = false;
+  #   description = "keyboard: layout tweaks and xcape";
+  #   wantedBy = [ "graphical.target" "default.target" ];
+  #   preStart = ''
+  #     ${pkgs.xorg.xkbcomp}/bin/xkbcomp ${compiledLayout} $DISPLAY
+  #   '';
+  #   restartIfChanged = true;
+  #   serviceConfig = {
+  #     Type = "forking";
+  #     Restart = "always";
+  #     RestartSec = 2;
+  #     ExecStart = "${pkgs.xcape}/bin/xcape -t 250 -e \'Shift_L=dollar;Shift_R=numbersign;Control_L=Escape;Control_R=Return\'";
+  #   };
+
+  # };
+
+  security.sudo.extraConfig = ''
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/poweroff
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/reboot
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/systemctl suspend
+    %wheel ALL=(ALL:ALL) ${pkgs.systemd}/bin/systemctl hibernate
+  '';
+
+  systemd.user.services.autorandrize = {
+    enable = true;
+    description = "Automatically adjust screens when waking up";
+    wantedBy = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+    after = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      TimeOutSec = "0";
+      ExecStart = "${pkgs.autorandr}/bin/autorandr -c";
+    };
+  };
 }
 
 #  LocalWords:  thomas dvp LocalWords
