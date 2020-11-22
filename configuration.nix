96874d84c54166cf2b9cee66aaf71721fbdf3972
Thomas Heartman <thomas.heartman+git@gmail.com>
2020-11-22 22:26:52 +0100

    Add required packages to exwm config.

--- configuration.nix
+++ configuration.nix
@@ -207,7 +207,7 @@ in {
     windowManager.exwm = {
       enable = true;
       enableDefaultConfig = false;
-      extraPackages = epkgs: [ epkgs.emacsql-sqlite ];
+      extraPackages = epkgs: [ epkgs.emacsql-sqlite epkgs.vterm epkgs.pdf-tools ];
     };
     displayManager.defaultSession = "none+exwm";
   };
