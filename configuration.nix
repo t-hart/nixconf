2111cde4db3ce4fe8d04eb027aa7742871b5375c
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-01-12 08:04:37 +0100

    Add picom config
    Copied from
    [here](https://github.com/nrdxp/nixflk/blob/f72125c5d233df778ea9de5b064c449239cdf486/profiles/graphical/xmonad/default.nix#L17-L24)
    and based on [this
    comment](https://discourse.nixos.org/t/getting-nvidia-to-work-avoiding-screen-tearing/10422/13)
    in a post about screen tearing I made on discourse.nixos.

--- configuration.nix
+++ configuration.nix
@@ -234,6 +234,15 @@ in
   # https://github.com/target/lorri/
   services.lorri.enable = true;
 
+  services.picom = {
+    enable = true;
+    inactiveOpacity = 0.8;
+    settings = {
+      "unredir-if-possible" = true;
+      "focus-exclude" = "name = 'slock'";
+    };
+  };
+
   services.xserver = {
     enable = true;
     libinput = {
