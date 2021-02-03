8e7f59fff27c468ce052039df7a1db0e98a05df1
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-02-03 08:52:50 +0100

    Activate blueman.

--- configuration.nix
+++ configuration.nix
@@ -195,6 +195,7 @@ in
   };
 
   hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   # uncomment this when moving to unstable or to stable after 21.03/05
   # hardware.keyboard.zsa.enable = true;
